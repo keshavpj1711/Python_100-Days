a799e472db565cfe29b09a02198b1c086b9d5819
keshavpj1711 <keshavpj1711@gmail.com>
2023-12-23 02:24:41 +0530

    Optimitized the code for Mail Merger Challenge further

--- Day-wise assortment/Intermediate/Day 24/Mail Merge Challenge/main.py
+++ Day-wise assortment/Intermediate/Day 24/Mail Merge Challenge/main.py
@@ -8,24 +8,23 @@
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-# Reading the file
-with open("./Input/Letters/starting_letter.txt", "r") as letter:
-    letter_lines = letter.readlines()  # Storing the lines as a list item
-
 # Reading the name list
 with open("./Input/Names/invited_names.txt", "r") as names:
     names_list = names.readlines()  # Storing all the names as list items
 
-# Stripping the names and replacing them with '[name]'
-for i in names_list:  # Traversing through the names_list to strip the names separately
+# Reading the file
+with open("./Input/Letters/starting_letter.txt", "r") as letter:
+    in_letter = letter.read()  # Storing the whole letter as a string
 
-    # Changing the [name] in letter _lines with the stripped name
-    letter_lines[0] = letter_lines[0].replace("[name]", i.strip())
+    # Stripping the names and replacing them with '[name]'
+    for i in names_list:  # Traversing through the names_list to strip the names separately
+        # Storing the stripped name
+        name = i.strip()
+        # Replacing [name] with name in letter_lines and storing the new letter in named_letter variable
+        named_letter = in_letter.replace("[name]", name)
 
-    # Finally writing the Output to a file name.txt
-    with open(f"./Output/ReadyToSend/{i.strip()}.txt", "w") as letters_ready:
-        for k in letter_lines:
-            letters_ready.write(k)
-
-    # Again changing the name back to [name] so that the first replace statement could work
-    letter_lines[0] = letter_lines[0].replace(i.strip(), "[name]")
+        # Finally writing the Output to a file name.txt
+        with open(f"./Output/ReadyToSend/{name}.txt", "w") as letters_ready:
+            letters_ready.write(named_letter)
+            # We can do this in a single line
+            # Since the named_letter has all the lines as a single string
