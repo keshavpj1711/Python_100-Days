152355f25886a2a5f0eeedb27b1ff67c1d0bf20e
keshavpj1711 <keshavpj1711@gmail.com>
2023-12-27 02:59:38 +0530

    added files for starting Day 26

---
+++ Day-wise assortment/Intermediate/Day 26/learning_about_comprehension/main.py
@@ -0,0 +1,9 @@
+# # List Comprehension
+# # Filtering out even numbers
+# numbers = [1, 4, 2, 9, 6, 3]
+# even_numbers = [num for num in numbers if num % 2 == 0]  # Filter, even numbers
+# print(even_numbers)
+# # Output: [4, 2, 6]
+
+
+# Dictionary Comprehension
