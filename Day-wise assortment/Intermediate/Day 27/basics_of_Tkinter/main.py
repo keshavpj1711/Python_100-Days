710e1f3bcd6fd1bf4b953fe5dad35a85f09e39b9
keshavpj1711 <keshavpj1711@gmail.com>
2023-12-29 03:58:16 +0530

    added learnings of buttons and entry in tkinter Day 28

--- Day-wise assortment/Intermediate/Day 27/basics_of_Tkinter/main.py
+++ Day-wise assortment/Intermediate/Day 27/basics_of_Tkinter/main.py
@@ -1,5 +1,12 @@
 import tkinter as tk
 
+
+# Defined functions
+def button_clicked():
+    # On executing this the user_input.get() gives us the text value in the entry
+    my_label["text"] = f"{user_input.get()}"
+
+
 # Creating our screen or say our window
 window = tk.Tk()
 
@@ -14,8 +21,27 @@ my_label = tk.Label(text="First Label", font=("Arial", 20, "normal"))
 # The font= argument here works in the same way as that in turtle
 # The packer function is used to place things on the screen
 # It's like the geometry mechanism
-my_label.pack(expand=True)
+my_label.pack(side="top")
+
+# Now, as we know, how these **kwargs work,
+# So, we can directly change the text in the label
+my_label["text"] = "Changed the label"
+
+# Creating a button
+button = tk.Button(text="Click Me", command=button_clicked)
+button.pack(side="top")
+# Currently this button does nothing
+# To make it functional we will make use of the command argument
+
+# Creating an entry component to our program
+user_input = tk.Entry(width=20)
+user_input.pack()
 
 # Keeping our windows open
 # This is to be kept at the end of the program
 tk.mainloop()
+
+# In this file we have so far worked with
+# window, window size
+# label, packer method
+# button and entry
