081b88f7d9d0027fe2b930993f96572134aa1dbb
keshavpj1711 <keshavpj1711@gmail.com>
2024-01-16 22:53:20 +0530

    trying to send my first message using smtplib Day 32

--- Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/main.py
+++ Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/main.py
@@ -1,1 +1,18 @@
 import smtplib
+
+# Initialising my email
+my_email = "test.user.python0520@gmail.com"
+my_password = "uergvgfvldwnfqxq"
+
+# Creating an obj that establishes connection with the mail server
+connection = smtplib.SMTP("smtp.gmail.com", port=587)
+# Starting TLS - Transport Layer Security, it's a way of securing connection with the mail server
+# Reason: So that no one can intercept our message, since tls encrypts our message
+connection.starttls()
+
+# Logging in
+connection.login(user=my_email, password=my_password)
+# Sending the message
+connection.sendmail(from_addr=my_email, to_addrs="testuser.python@yahoo.com", msg="Hello")
+# Closing the connection
+connection.close()
