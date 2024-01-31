ef5a8b0431060e2dc967118222a4698f71dbd1f4
keshavpj1711 <keshavpj1711@gmail.com>
2024-01-31 18:42:13 +0530

    first msg sent using smtplib

--- Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/main.py
+++ Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/main.py
@@ -2,7 +2,7 @@ import smtplib
 
 # Initialising my email
 my_email = "test.user.python0520@gmail.com"
-my_password = "uergvgfvldwnfqxq"
+my_password = "ezyjbzaszosbhner"
 
 # Creating an obj that establishes connection with the mail server
 connection = smtplib.SMTP("smtp.gmail.com", port=587)
@@ -13,6 +13,9 @@ connection.starttls()
 # Logging in
 connection.login(user=my_email, password=my_password)
 # Sending the message
-connection.sendmail(from_addr=my_email, to_addrs="testuser.python@yahoo.com", msg="Hello")
+connection.sendmail(
+    from_addr=my_email,
+    to_addrs="crueser123@gmail.com",
+    msg="Subject:Hello\n\nHi, this is my first msg using the SMTP module.")
 # Closing the connection
 connection.close()
