03d1e8496a60ddc689cd9d4f9cac407ad34d56dc
keshavpj1711 <keshavpj1711@gmail.com>
2024-01-31 19:15:30 +0530

    check for required week day done

--- Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/quote_of_the_day/quote_of_the_day.py
+++ Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/quote_of_the_day/quote_of_the_day.py
@@ -5,3 +5,21 @@
 # 2. Open the quotes.txt file and obtain a list of quotes.
 # 3. Use the random module to pick a random quote from your list of quotes.
 # 4. Use the smtplib to send the email to yourself.
+
+import datetime as dt
+import smtplib
+
+
+# Here we will be writing a function that returns a random quote from quote.txt
+
+
+# Checking for weekday to be Wednesday
+# Getting the current info
+now = dt.datetime.now()
+# Getting today's weekday
+week_day = now.weekday()
+
+if week_day == 2:
+    print("Today's Wednesday")
+else:
+    pass
