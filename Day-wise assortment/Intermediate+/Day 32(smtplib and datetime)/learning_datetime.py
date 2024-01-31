87abe46d10348dc759a9b9ebd61d164d730d1c5e
keshavpj1711 <keshavpj1711@gmail.com>
2024-01-31 19:07:57 +0530

    completed learning datetime module, starting with quote_of_the_day program

--- Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/learning_datetime.py
+++ Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/learning_datetime.py
@@ -6,4 +6,8 @@ year = now.year
 month = now.month
 day = now.day
 week_day = now.weekday()  # Note 0 stands for Monday and so on
-print(week_day)
+# print(week_day)
+
+# Creating our own object
+my_dob = dt.datetime(day=17, month=11, year=2003)
+print(my_dob)
---
+++ Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/quote_of_the_day/quote_of_the_day.py
@@ -0,0 +1,7 @@
+# Objective: send a motivational quote via email on the current
+# weekday (you can change it to Monday afterward)
+# Hints
+# 1. Use the datetime module to obtain the current day of the week.
+# 2. Open the quotes.txt file and obtain a list of quotes.
+# 3. Use the random module to pick a random quote from your list of quotes.
+# 4. Use the smtplib to send the email to yourself.
