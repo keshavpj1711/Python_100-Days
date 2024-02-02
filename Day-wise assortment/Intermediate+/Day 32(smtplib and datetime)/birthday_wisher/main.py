63c08440231f4e6c60b0ba92eded8eabd38c7a57
keshavpj1711 <keshavpj1711@gmail.com>
2024-02-02 15:25:04 +0530

    completed birthday_wisher

--- Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/birthday_wisher/main.py
+++ Day-wise assortment/Intermediate+/Day 32(smtplib and datetime)/birthday_wisher/main.py
@@ -1,19 +1,10 @@
-##################### Extra Hard Starting Project ######################
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
 import smtplib
 import datetime as dt
 import pandas as pd
 import random
 
 
-def send_msg(wish, name, msg):
+def send_msg(wish, msg):
     # Initialising my email
     my_email = "test.user.python0520@gmail.com"
     my_password = "ezyjbzaszosbhner"
@@ -30,29 +21,43 @@ def send_msg(wish, name, msg):
         connection.sendmail(
             from_addr=my_email,
             to_addrs="crueser123@gmail.com",
-            msg=f"Subject:{wish} {name}\n\n{msg}")
+            msg=f"Subject:{wish}\n\n{msg}")
 
 
 # Opening and checking the file
 birthday_data = pd.read_csv("birthdays.csv")
-email_column = birthday_data["email"]
-name_column = birthday_data["name"]
-year_column = birthday_data["year"]
-month_column = birthday_data["month"]
-day_column = birthday_data["day"]
 
-
+# Getting today's info
 now = dt.datetime.now()
 year = now.year
 month = now.month
 day = now.day
 
+# Iterating through rows in pandas
+# Read the CSV file into a DataFrame
+# df = pd.read_csv("your_file.csv")
+# for index, row in df.iterrows():
+#     # Access row values by column name
+#     name = row['Name']
+#     age = row['Age']
+#     # Process the row data as needed
+#     print(f"Name: {name}, Age: {age}")
 
-# Random Number
-random_no = random.randint(1, 3)
+for index, row in birthday_data.iterrows():
 
-with open(f"letter_templates/letter_{random_no}.txt") as letter_format:
-    letter = letter_format.read()
+    # Random Number
+    random_no = random.randint(1, 3)
 
-for i in birthday_data:
-    pass
+    # Getting a random letter format
+    with open(f"letter_templates/letter_{random_no}.txt") as letter_format:
+        letter = letter_format.read()
+
+    # Checking if today is a birthday or not
+    if [day, month, year] == [row["day"], row["month"], row["year"]]:
+        # Getting the name on the letter
+        name = row["name"]
+        letter = letter.replace("[NAME]", name)
+        # print(letter)
+
+        send_msg("Happy Birthday", letter)
+
