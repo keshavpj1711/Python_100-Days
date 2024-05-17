e3c63bfb857ee94d05c9da17ebb1177bd932ea71
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-18 05:05:58 +0530

    completed with getting the required data to check if it'll be visible when overhead

--- Intermediate+/33IssLocation(APIs)/ISS_overhead_notif/main.py
+++ Intermediate+/33IssLocation(APIs)/ISS_overhead_notif/main.py
@@ -1,10 +1,12 @@
 import requests
+from datetime import datetime
 
 # These are the parameters we need to pass during api call
 parameters = {
     # Lats and Longs for your location 
     "lat":28.644800,
     "lng":77.216721,
+    "formatted":0, # To get 24 hour clock format
     "tzid":"Asia/Kolkata"
 }
 
@@ -15,5 +17,16 @@ response = requests.get(url="https://api.sunrise-sunset.org/json", params=parame
 response.raise_for_status()
 
 data = response.json()
-print(data)
+# print(data)
 
+# Getting sunrise and sunset times
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+curr_time = datetime.now()
+
+# Now our task is to just compare the current hour against sunset and sunrise hour
+# So first we need to get hold of just the hour 
+sunrise = sunrise.split("T")[1].split(":")[0]
+sunset = sunset.split("T")[1].split(":")[0]
+curr_time = curr_time.hour
