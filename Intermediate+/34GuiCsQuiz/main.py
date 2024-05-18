52ce9dadd4ea53e4453669efbe6ba96307b0fb9a
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-18 18:12:22 +0530

    started setting up screen for gui_quiz

--- Intermediate+/34GuiCsQuiz/main.py
+++ Intermediate+/34GuiCsQuiz/main.py
@@ -3,6 +3,24 @@
     # Next we need to revise Tkinter and 
     
 from quiz_ques import GetQuestions
+import tkinter as tk
 
+# Creating a window
+screen = tk.Tk()
 
+# Setting up title 
+screen.title("Tech Quiz")
+# Setting window size
+screen.geometry('400x400')
+#Setting bg color
+screen.configure(background="light green")
 
+# What things we need 
+# Two labels
+    # One for maintaining score
+    # Other for Showing the question
+# Two buttons
+    # True and,
+    # False
+
+screen.mainloop()
