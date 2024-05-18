f7c8f860ae8f8c8db41760b62af1ad338f130d1d
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-18 21:56:00 +0530

    small additions to make code readable

--- Intermediate+/34GuiCsQuiz/main.py
+++ Intermediate+/34GuiCsQuiz/main.py
@@ -4,13 +4,15 @@
     
 from quiz_ques import GetQuestions
 from quiz_brain import QuizBrain
-import tkinter as tk
 
+# Creating the respective obejcts to be used 
 quiz_questions = GetQuestions().questions_list
 quiz_brain = QuizBrain(quiz_questions)
 
+# Basically loops till there are questions present in the data
 for i in range(0, len(quiz_questions)):
     quiz_brain.next_question()
     quiz_brain.check_answer()
     quiz_brain.show_score()
 
+# Our next target is to get everything to GUI and that too TKinter in Classes
