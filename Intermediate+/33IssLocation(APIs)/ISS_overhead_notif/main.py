774a1847ad2359c63d3866d70751111db470738b
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-18 05:32:48 +0530

    added the condition for when to send email

--- Intermediate+/33IssLocation(APIs)/ISS_overhead_notif/main.py
+++ Intermediate+/33IssLocation(APIs)/ISS_overhead_notif/main.py
@@ -1,11 +1,18 @@
 import requests
 from datetime import datetime
 
+MY_LAT = 28.644800
+MY_LNG = 77.216721
+
+def sendsms(email):
+    # Basically this function sends and ISS Overhead Notification to the input email
+    pass
+
 # These are the parameters we need to pass during api call
 parameters = {
     # Lats and Longs for your location 
-    "lat":28.644800,
-    "lng":77.216721,
+    "lat":MY_LAT,
+    "lng":MY_LNG,
     "formatted":0, # To get 24 hour clock format
     "tzid":"Asia/Kolkata"
 }
@@ -27,6 +34,26 @@ curr_time = datetime.now()
 
 # Now our task is to just compare the current hour against sunset and sunrise hour
 # So first we need to get hold of just the hour 
-sunrise = sunrise.split("T")[1].split(":")[0]
-sunset = sunset.split("T")[1].split(":")[0]
+sunrise = int(sunrise.split("T")[1].split(":")[0])
+sunset = int(sunset.split("T")[1].split(":")[0])
 curr_time = curr_time.hour
+
+
+# Our next task is if it's dark and location of iss is near our viewpoint 
+# Then send us an email
+if (sunset < curr_time < sunrise):
+    # Then only start looking if ISS is above us or not
+
+    # Getting location from ISS Location API
+    iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    iss_response.raise_for_status()
+
+    iss_data = iss_response.json()
+    iss_lat = float(iss_data["iss_position"]["latitude"])
+    iss_lng = float(iss_data["iss_position"]["longitude"])
+
+    # Checking if ISS is +- 5 deg around us 
+    if (MY_LAT-5 < iss_lat < MY_LAT+5 and MY_LNG-5 < iss_lng < MY_LNG+5):
+        # Send and email to notify
+        sendsms("crueser123@gmail.com")
+
