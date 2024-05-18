4af779419e81cd5a2bfee2f0fdef1b0d3c962986
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-19 02:30:54 +0530

    got to showning ques on gui moving on to working of buttons

--- Intermediate+/34GuiCsQuiz/quiz_brain.py
+++ Intermediate+/34GuiCsQuiz/quiz_brain.py
@@ -8,16 +8,15 @@ class QuizBrain:
         self.user_score = 0
         self.user_choice = ""
         self.current_question = [] 
-        self.questions_remaining = len(question_list)
         # the data stored will be of format [question, correct answer]
 
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.q_text = self.current_question[0]
         self.q_ans = self.current_question[1]
-        self.user_choice = input(f"Q{self.question_number+1}. {self.q_text} (T/F)?: ")
+        # self.user_choice = input(f"Q{self.question_number+1}. {self.q_text} (T/F)?: ")
         self.question_number += 1
-        self.questions_remaining -= 1
+        return self.current_question
 
     def check_answer(self):
         if self.current_question[1][0] == self.user_choice:
--- Intermediate+/34GuiCsQuiz/ui.py
+++ Intermediate+/34GuiCsQuiz/ui.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizGUI:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        # By doing this: "quiz_brain: QuizBrain"
+        # basically we define the data type of the parameter that we want
+        self.quiz_brain = quiz_brain
         
         self.window = tk.Tk()
         self.window.title("QuizApp")
@@ -17,15 +21,26 @@ class QuizGUI:
 
         # Creating a canvas
         self.question_space = tk.Canvas(bg="white", height=250, width=300)
-        self.question_text = self.question_space.create_text(150, 125, text="Question", font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.question_text = self.question_space.create_text(150, 125, text="Question", font=("Arial", 20, "italic"), fill=THEME_COLOR, width=280)
         self.question_space.grid(column=0, row=1, columnspan=2, pady=50)
 
         # Adding the two required buttons
-        red_cross_img = tk.PhotoImage(file=r"./images/red_cross.png")
-        green_tick_img = tk.PhotoImage(file=r"./images/green_tick.png")
-        self.false_button = tk.Button(height=50, width=50, image=red_cross_img, highlightthickness=0)
+        self.false_button = tk.Button(height=50, width=50, text="FALSE", highlightthickness=0)
         self.false_button.grid(column=0, row=2) 
-        self.true_button = tk.Button(height=50, width=50, image=green_tick_img, highlightthickness=0)
+        self.true_button = tk.Button(height=50, width=50, text="TRUE", highlightthickness=0)
         self.true_button.grid(column=1, row=2)
 
+        # Calling next question
+        self.get_next_question()
+
         self.window.mainloop()
+
+    
+    def get_next_question(self):
+        q_text = self.quiz_brain.next_question()[0] 
+        # Since this returns list with question at 0 as well as ans at 1 index
+
+        # Changing text in canvas
+        self.question_space.itemconfig(self.question_text, text=f"{q_text}")
+
+    def true_clicked(self):
