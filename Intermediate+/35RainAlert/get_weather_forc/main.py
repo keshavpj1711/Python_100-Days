0bfe771a2409df9f03b49cebac89029b2034b87a
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-19 21:15:51 +0530

    made it look a bit clean

--- Intermediate+/35RainAlert/get_weather_forc/main.py
+++ Intermediate+/35RainAlert/get_weather_forc/main.py
@@ -24,6 +24,11 @@ temp_data = data["main"]
 visibility_data = data["visibility"]
 wind_data = data["wind"]
 
-print(coord_data)
-print(temp_data)
+print("----------WEATHER-FORECAST----------")
+print(f"Your coordinates are, \nlon: {coord_data["lon"]}\nlat: {coord_data["lat"]}")
+print(f"Current Weather: {weather_data[0]["main"]}\nDescription: {weather_data[0]["description"]}")
+print(f'''Current temperature: {temp_data["temp"]} deg C
+Feels Like: {temp_data["feels_like"]} deg C
+Humidity: {temp_data["humidity"]}%'''
+)
 
