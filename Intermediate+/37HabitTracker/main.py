90b317b5eee8d8662e5e1543d4ec6333da7c128c
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-20 22:55:00 +0530

    configured the first pixela graph

--- Intermediate+/37HabitTracker/main.py
+++ Intermediate+/37HabitTracker/main.py
@@ -5,12 +5,15 @@ from dotenv import load_dotenv
 # Basically loading those environment variables which are to be used
 load_dotenv()
 
+USERNAME = str(os.environ.get("pixela_username"))
+TOKEN = str(os.environ.get("pixela_token"))
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 # Setting up our pixela account
 user_param = {
-    "token": str(os.environ.get("pixela_token")),
-    "username": str(os.environ.get("pixela_username")),
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
@@ -18,3 +21,17 @@ user_param = {
 # To setup user account 
 # response = requests.post(url=pixela_endpoint, json=user_param)
 # print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+# Cofiguring your graph
+graph_config = {
+    "id": "graph01",
+    "name": "ReadingG",
+    "unit": "pages",
+    "type": "int",
+    "color": "momiji"
+}
+
+response = requests.post(url=graph_endpoint, params=graph_config)
+
