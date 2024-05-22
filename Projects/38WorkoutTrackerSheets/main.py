d2aacd930bffd5dc3bbba88e9d23c299bc7216aa
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-23 04:20:56 +0530

    trying to get the requested output from nutritionix api

--- Projects/38WorkoutTrackerSheets/main.py
+++ Projects/38WorkoutTrackerSheets/main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 # loading the .env file to access our environment variables
 load_dotenv()
 
+host_domain = "https://trackapi.nutritionix.com"
 lang_for_exercise_endpoint = "/v2/natural/exercise"
 
 # defining headers to auth our api request
@@ -12,3 +13,20 @@ header = {
     "x-app-id": os.environ.get("app_id"),
     "x-app-key": os.environ.get("api_key")
 }
+
+# Getting the response from the user 
+user_input = input("Which Exercise you did: ")
+
+# Setting the parameters 
+parameters = {
+    "query": user_input,
+}
+
+# Making a request
+response = requests.post(url=f"{host_domain}{lang_for_exercise_endpoint}", params=parameters, headers=header)
+response.raise_for_status() # Raise exceptions if any
+
+# Checking the data we recieved 
+data = response.json()
+print(data)
+
