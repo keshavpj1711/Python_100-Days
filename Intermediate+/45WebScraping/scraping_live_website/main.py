92ce0446f3ef30ba96f7add7e77008c43573e18d
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-25 03:51:30 +0530

    finally solved how to get upvotes for the the blogs and also how to check whether upvotes are zero or not

--- Intermediate+/45WebScraping/scraping_live_website/main.py
+++ Intermediate+/45WebScraping/scraping_live_website/main.py
@@ -13,6 +13,36 @@ soup = BeautifulSoup(html_data, "html.parser")
 # Getting the titles and links of all the top articles
 
 # Getting hold of titles
-titles = soup.select(".titleline a")
-for i in titles:
+# So first we got span tag which includes 
+# the anchor tag in which we are intrested in
+row_entry = soup.find_all("tr", class_="athing")
+row_entry_upvotes = soup.select("tr .subtext")
+# After that we loop through the text and 
+# filter out the text from the first anchor tag 
+# as it contains the titles for them
+blogs_list = [] # Each element will be a dict with blog_name and blog_link as keys
+for i in range(len(row_entry)):
+
+    if row_entry_upvotes[i].find("span", class_="subline") == None:
+        upvotes = "0 points"
+    else:
+        upvotes_entry = row_entry_upvotes[i].select_one(".subline .score")
+        upvotes = upvotes_entry.getText()
+    # print(upvotes) 
+
+    span = row_entry[i].select(".title .titleline")
+    # print(span)
+    blog_text = span[0].find('a').getText()
+    blog_link = span[0].find('a').get("href")
+    blogs_list.append(
+        {
+            "blog_text": blog_text,
+            "blog_link": blog_link,
+            "blog_upvotes": upvotes,
+        }
+    )
+    print(blogs_list[i])
+
+
+for i in blogs_list:
     print(i)
