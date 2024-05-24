f73a4f78a29d934ebc453295fb5feebddcfac487
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-24 22:04:47 +0530

    some basic stuff about selecting elements added

--- Intermediate+/45WebScraping/bs4-start/main.py
+++ Intermediate+/45WebScraping/bs4-start/main.py
@@ -12,6 +12,17 @@ soup = BeautifulSoup(contents, "html.parser")
 # For some websites html.parser might not work in that case 
 # We might have to use lxml
 
+# Getting title tag 
+print(soup.title)
+# Getting the string of title
+print(soup.title.string)
 
+# Getting hold of a single element
+print(soup.select_one("p a")) 
+print(soup.select_one("#name").string)
+# This is basically selecting the elements like we do in CSS
+# Also if you want to select more than one element we have to use 
+print(soup.select(".heading"))
+# [<h3 class="heading">Books and Teaching</h3>, <h3 class="heading">Other Pages</h3>]
 
 
