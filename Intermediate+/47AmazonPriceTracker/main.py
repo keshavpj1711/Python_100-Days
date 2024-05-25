d55a7cdd1746607411ee761aff3ee3fce774e7e6
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-25 21:17:30 +0530

    setting up argv for tracker

---
+++ Intermediate+/47AmazonPriceTracker/main.py
@@ -0,0 +1,17 @@
+# Functionalities i want 
+    # I want it to ask for the link of the products.
+    # Then it should store that info from in some file by also scraping the name from it. 
+    # It should ask for user input of alert price 
+    # Next it should check all the listed stuff in the file for the target price if reached
+
+import requests
+import csv
+import sys
+
+
+def main():
+
+    # Taking inputs 
+    product_link = input("Enter your product link")
+
+
