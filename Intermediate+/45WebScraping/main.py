d52b6b844e1b3afb75087a4cfb1eb38b3f01768f
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-25 14:34:46 +0530

    scrapped top 100 movies from empireonline.com

---
+++ Intermediate+/45WebScraping/main.py
@@ -0,0 +1,23 @@
+# To scrape top 100 movies from https://www.empireonline.com/movies/features/best-movies-2/
+
+from os import write
+from types import resolve_bases
+from bs4 import BeautifulSoup
+import requests
+
+# Getting response and html data
+response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+response.raise_for_status()
+# Html data 
+html_data = response.text
+
+# Making soup 
+soup = BeautifulSoup(html_data, "html.parser")
+
+# Getting the movie titles
+movie_title_tags = soup.find_all("h3", class_="listicleItem_listicle-item__title__BfenH")
+
+with open("top100_movie_list.txt", "w") as movie_list:
+    for tag in movie_title_tags:
+        movie_list.write(f"{tag.getText()}\n")
+    
