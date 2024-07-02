ed5d0bb956541d061c07507008d9f46774eb90fc
Keshav Prajapati <keshavpj1711@gmail.com>
2024-07-02 23:21:12 +0530

    understanding __main__ and setting up easy way to run flask app

--- WebDev/54FlaskIntro/hello_flask/app.py
+++ WebDev/54FlaskIntro/hello_flask/app.py
@@ -1,7 +1,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+# This __name__ is basically "__main__"
+# __main__ basically provides an entry point to your code just like main() in other langs
+
+# __main__
+# In Python,
+# __main__ is a variable that can hold different values depending on how the script is executed. 
+# It's set to '__main__' when the script is run directly, but a different value (usually the script's module name) when it's imported as a module.
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# To Run our app directly we can do this
+if __name__ == "__main__":
+    app.run()
