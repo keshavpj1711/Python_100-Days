d473f379aa8b9dea9ca98a00946637688aa605d1
Keshav Prajapati <keshavpj1711@gmail.com>
2024-07-02 23:49:54 +0530

    adding another decorator function for different path in our web app

--- WebDev/54FlaskIntro/hello_flask/app.py
+++ WebDev/54FlaskIntro/hello_flask/app.py
@@ -9,9 +9,15 @@ app = Flask(__name__)
 # __main__ is a variable that can hold different values depending on how the script is executed. 
 # It's set to '__main__' when the script is run directly, but a different value (usually the script's module name) when it's imported as a module.
 
+# This is a decorator function which only executes the below function when '/' is accessed
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# Creating another decorator
+@app.route("/bye")
+def bye_bye():
+    return "<p>Bye Bye!!</p>"
 
 # To Run our app directly we can do this
 if __name__ == "__main__":
