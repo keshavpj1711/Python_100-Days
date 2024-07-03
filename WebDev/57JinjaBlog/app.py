3eb56608f4cef324b3af3bed32fa634a3f774d0b
Keshav Prajapati <keshavpj1711@gmail.com>
2024-07-04 01:38:40 +0530

    updating copyright footer with current year using datetime module

--- WebDev/57JinjaBlog/app.py
+++ WebDev/57JinjaBlog/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from datetime import date
+
+# Getting current year
+current_year = date.today().year
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html", curr_year=current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
