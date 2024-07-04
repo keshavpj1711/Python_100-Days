2f766fcef4bbde8f0b56a7b806f0915062c106f6
Keshav Prajapati <keshavpj1711@gmail.com>
2024-07-04 19:48:37 +0530

    created post.py to handle getting posts and updated app with rendering post.html

--- WebDev/57JinjaBlog/app.py
+++ WebDev/57JinjaBlog/app.py
@@ -1,22 +1,19 @@
 from flask import Flask, render_template
-import requests
+# Calling class
+from post import Post
+
+# Creating instance of that class
+posts = Post()
 
 app = Flask(__name__)
 
-# Getting blogs from ou api 
-def get_blogs():
-    api_endpoint = "https://api.npoint.io/c790b4d5cab58020d391"
-
-    response = requests.get(url=api_endpoint)
-    response.raise_for_status()
-
-    blog_data = response.json()
-
-    return blog_data
-
 @app.route("/")
 def home_page():
-    return render_template("index.html", blogs=get_blogs())
+    return render_template("index.html", blogs=posts.get_blogs())
+
+@app.route("blog/<id>")
+def blogs(id):
+    return render_template("post.html", blog_id=id)
 
 if __name__ == "__main__":
     app.run(debug=True)
