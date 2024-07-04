003dee56d20bbd7505beb0dcf5a7a95703049351
Keshav Prajapati <keshavpj1711@gmail.com>
2024-07-04 23:54:20 +0530

    working to get each post running seperately

--- WebDev/57JinjaBlog/app.py
+++ WebDev/57JinjaBlog/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+import requests
+
+# Getting blog post data
+api_endpoint = "https://api.npoint.io/c790b4d5cab58020d391"
+blogs_data = requests.get(url=api_endpoint).json()
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html", blogs=blogs_data)
+
+
+@app.route("/blog/<blog_id>")
+def show_blog(blog_id):
+    return render_template("post.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
